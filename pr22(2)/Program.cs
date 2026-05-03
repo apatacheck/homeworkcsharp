a02809446f77f54436615b3754ed20f618fa82f3
ksenon <apatacheck@mail.ru>
2026-05-04 03:52:55 +0400

    nrew

---
+++ pr22(2)/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace pr22_2_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Graph g = new Graph("input.txt");
+            Console.WriteLine("Граф:");
+            g.Show();
+            Console.WriteLine();
+            Console.Write("Введите L: ");
+            int L = int.Parse(Console.ReadLine()!);
+            g.PrintPairsWithinDistance(L);
+
+            Console.ReadKey();
+        }
+    }
+}
