1c11f120f275c7be495a14bd89686a080f8a549e
ksenon <apatacheck@mail.ru>
2026-05-09 00:55:26 +0400

    pr22(3)

---
+++ pr22(3)/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace pr22_3_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Graph g = new Graph("input.txt");
+
+            Console.WriteLine("Граф:");
+            g.Show();
+            Console.WriteLine();
+
+            g.ShortestPath("Москва", "Калуга", "Владимир");
+
+            Console.ReadKey();
+        }
+    }
+}
